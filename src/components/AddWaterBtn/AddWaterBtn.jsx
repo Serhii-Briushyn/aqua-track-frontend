17415f27881da1dea8614c4852905adb92e03783
Andrey Drapatiy <drapatiy92@gmail.com>
2025-01-09 19:22:49 +0200

    Water Item

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,14 +1,14 @@
 import WaterModal from "../WaterModal/WaterModal";
 import css from "./AddWaterBtn.module.css";
-import { GoPlus } from "react-icons/go";
+import icons from "../../assets/icons/icons.svg";
 
 const AddWaterBtn = () => {
   return (
     <div className={css.addWaterBtn}>
       <button role="button" onClick={() => {}} className={css.btn}>
-        <div className={css.iconPlus}>
-          <GoPlus />
-        </div>
+        <svg width="30" height="30">
+          <use href={`${icons}#icon-plus-circle-green`} />
+        </svg>
         <span className={css.btnTxt}>Add Water</span>
       </button>
       {/*<WaterModal/>*/}
--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -1,10 +1,30 @@
 import DeleteWaterModal from "../DeleteWaterModal/DeleteWaterModal";
 import WaterModal from "../WaterModal/WaterModal";
 import css from "./WaterItem.module.css";
+import icons from "../../assets/icons/icons.svg";
 const WaterItem = () => {
   return (
     <div className={css.waterItem}>
-      wqerqwe
+      <svg width="45" height="45">
+        <use href={`${icons}#icon-glass`} />
+      </svg>
+
+      <div className={css.indicators}>
+        <span className={css.volume}>250 ml</span>
+        <span className={css.time}>11:00 AM</span>
+      </div>
+      <div className={css.actions}>
+        <button role="button">
+          <svg width="16" height="16">
+            <use href={`${icons}#icon-edit`} />
+          </svg>
+        </button>
+        <button role="button">
+          <svg width="16" height="16">
+            <use href={`${icons}#icon-delete`} />
+          </svg>
+        </button>
+      </div>
       {/*<WaterModal />*/}
       {/*<DeleteWaterModal/>*/}
     </div>
