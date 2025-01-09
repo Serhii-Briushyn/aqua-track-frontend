bfe1de1d44953f6995e6f090407c2bc6e9741406
xPhoenixUAx <pavloff12@gmail.com>
2025-01-10 00:38:36 +0200

    check of working

---
+++ src/components/WaterForm/WaterForm.jsx
@@ -0,0 +1,167 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { IconPlus } from "../../components/Icons/IconPlus";
+import { IconMinus } from "../Icons/IconMinus";
+
+import { getCurrentTime } from "../../helpers/getCurrentTime";
+
+import css from "./WaterForm.module.css";
+
+import { convertTimeToUnix } from "../../helpers/convertTimeToUnix";
+import { useDispatch, useSelector } from "react-redux";
+import { addWater, putWater } from "../../redux/water/waterOps";
+import toast from "react-hot-toast";
+import { selectWaterDate } from "../../redux/selectors";
+
+const schema = yup.object().shape({
+  date: yup.string().required("Please, enter the recorded time"),
+  amount: yup
+    .number()
+    .min(10, "Amount of water must be greater than 10 ml")
+    .max(2000, "Amount of water must be less than 2000 ml")
+    .typeError("Enter a valid amount of water in ml")
+    .required("Value is required"),
+});
+
+const formatToLocalTime = (timestamp) => {
+  const date = new Date(timestamp);
+  const offset = date.getTimezoneOffset() * 60000;
+  const localDate = new Date(date.getTime() - offset);
+  return localDate.toISOString().slice(11, 16);
+};
+
+export const WaterForm = ({ subtitle, onClose, toggleHandle, water }) => {
+  const dispatch = useDispatch();
+
+  const timezoneOffset = new Date().getTimezoneOffset();
+
+  const offset = timezoneOffset * 60 * 1000;
+
+  const day = useSelector(selectWaterDate);
+
+  const defaultValues = water
+    ? {
+        date: formatToLocalTime(water.date + offset),
+        amount: water.amount,
+      }
+    : {
+        date: getCurrentTime(),
+        amount: 50,
+      };
+
+  const {
+    register,
+    setValue,
+    getValues,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues,
+  });
+
+  const submitForm = async (data) => {
+    const time = data.date;
+
+    const unixTime = convertTimeToUnix(time, day) - offset;
+
+    if (unixTime > Date.now() - offset) {
+      toast.error("You can't choose a date in the future."),
+        {
+          duration: 5000,
+          position: "top-center",
+          style: {
+            textAlign: "center",
+            boxShadow: "8px 11px 27px -8px rgba(66, 68, 90, 1)",
+          },
+        };
+      return;
+    }
+
+    const dataToSend = {
+      amount: data.amount,
+      date: unixTime,
+    };
+
+    const response = toggleHandle
+      ? await dispatch(addWater(dataToSend))
+      : await dispatch(putWater([water._id, dataToSend]));
+
+    response.meta.requestStatus === "fulfilled" && onClose();
+  };
+
+  const subtract = () => {
+    const currentValue = getValues("amount");
+    setValue("amount", currentValue - 50);
+  };
+
+  const add = () => {
+    const currentValue = getValues("amount");
+    setValue("amount", currentValue + 50);
+  };
+
+  return (
+    <form className={css.formWrapper} onSubmit={handleSubmit(submitForm)}>
+      <p className={css.subtitle}>{subtitle}</p>
+      <span className={css.amountTitle}>Amount of water:</span>
+      <div className={css.amountWrapper}>
+        <button
+          className={css.amountButton}
+          onClick={subtract}
+          type="button"
+          disabled={getValues("amount") === 0 ? true : false}
+        >
+          <IconMinus className={css.icon} />
+        </button>
+
+        <span className={css.amountValue}>{`${watch("amount")} ml`}</span>
+
+        <button
+          className={css.amountButton}
+          onClick={add}
+          type="button"
+          disabled={getValues("amount") === 5000 ? true : false}
+        >
+          <IconPlus className={css.icon} />
+        </button>
+      </div>
+
+      <div className={css.fieldsWrapper}>
+        <label className={css.timeLabel} htmlFor="date">
+          Recording time:
+          {errors.date && (
+            <span className={css.validationError}>{errors.date.message}</span>
+          )}
+        </label>
+        <input
+          {...register("date")}
+          className={css.input}
+          type="time"
+          name="date"
+          id="date"
+        />
+
+        <label className={css.valueLabel} htmlFor="value">
+          Enter the value of the water used:
+          {errors.amount && (
+            <span className={css.validationError}>{errors.amount.message}</span>
+          )}
+        </label>
+        <input
+          {...register("amount")}
+          className={css.input}
+          type="number"
+          name="value"
+          id="value"
+          onChange={(e) => setValue("amount", Number(e.target.value))}
+        />
+      </div>
+      <button className={css.submitButton} type="submit">
+        Save
+      </button>
+    </form>
+  );
+};
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,48 +1,22 @@
-import { useState } from "react";
-import s from "./WaterModal.module.css";
-const WaterModal = () => {
-  const [amount, setAmount] = useState(50);
+import { WaterForm } from "../WaterForm/WaterForm";
+import css from "./WaterModal.module.css";
 
-  const handleDecrease = () => {
-    if (amount > 0) {
-      setAmount(amount - 10);
-    }
-  };
-
-  const handleIncrease = () => {
-    setAmount(amount + 10);
-  };
+export const WaterModal = ({
+  title,
+  subtitle,
+  onClose,
+  toggleHandle,
+  water,
+}) => {
   return (
-    <div className={s.modal}>
-      <span className={s.close} onClick={() => console.log("Close modal")}>
-        ×
-      </span>
-      <h2>Add water</h2>
-
-      <div className={s.field}>
-        <label>Choose a value:</label>
-        <div className={s.amountControls}>
-          <button onClick={handleDecrease}>-</button>
-          <span>{amount} ml</span>
-          <button onClick={handleIncrease}>+</button>
-        </div>
-      </div>
-
-      <div className={s.field}>
-        <label>Recording time:</label>
-        <input type="text" value="17:42" readOnly />
-      </div>
-
-      <div className={s.field}>
-        <label>Enter the value of the water used:</label>
-        <input type="text" value={amount} readOnly />
-      </div>
-
-      <button className={s.save} onClick={() => console.log("Save data")}>
-        Save
-      </button>
+    <div className={css.wrapper}>
+      <h2 className={css.title}>{title}</h2>
+      <WaterForm
+        subtitle={subtitle}
+        onClose={onClose}
+        toggleHandle={toggleHandle}
+        water={water}
+      />
     </div>
   );
 };
-
-export default WaterModal;
