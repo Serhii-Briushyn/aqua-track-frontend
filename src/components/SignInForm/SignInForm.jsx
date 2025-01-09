99004c08cedb1a5d0536cee92b86e3b95c5d0a34
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2025-01-09 18:46:11 +0100

    add wrapper

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import s from "./SignInForm.module.css";
 import { useState } from "react";
 import icons from "../../assets/icons/icons.svg";
+import Logo from "../Logo/Logo";
 
 const SignInForm = () => {
   //   const dispatch = useDispatch()
@@ -29,52 +30,55 @@ const SignInForm = () => {
   };
 
   return (
-    <div className={s.login}>
-      <h2 className={s.title}>Sign In</h2>
-      <Formik>
-        <Form className={s.form}>
-          <ul className={s.list}>
-            <li>
-              <label className={s.label}>
-                Email
-                <Field
-                  name="email"
-                  type="email"
-                  className={s.input}
-                  placeholder="Enter your email"
-                />
-              </label>
-            </li>
-            <li>
-              <label className={`${s.password} ${s.label}`}>
-                Password
-                <Field
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  className={`${s.password} ${s.input}`}
-                  placeholder="Enter your password"
-                />
-                <svg className={s.icon} onClick={togglePasswordVisibility}>
-                  <use
-                    href={`${icons}#${
-                      showPassword ? "icon-view" : "icon-hide"
-                    }`}
+    <div className={s.signin_wrapper}>
+      <Logo />
+      <div className={s.signin_form}>
+        <h2 className={s.title}>Sign In</h2>
+        <Formik>
+          <Form className={s.form}>
+            <ul className={s.list}>
+              <li>
+                <label className={s.label}>
+                  Email
+                  <Field
+                    name="email"
+                    type="email"
+                    className={s.input}
+                    placeholder="Enter your email"
                   />
-                </svg>
-              </label>
-            </li>
-          </ul>
-          <button className={s.btn} type="submit">
-            Sing in
-          </button>
-          <div className={s.no_account}>
-            <p className={s.text}>Don’t have an account?</p>
-            <Link to="/signup" className={s.link} type="submit">
-              Sign Up
-            </Link>
-          </div>
-        </Form>
-      </Formik>
+                </label>
+              </li>
+              <li>
+                <label className={`${s.password} ${s.label}`}>
+                  Password
+                  <Field
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    className={`${s.password} ${s.input}`}
+                    placeholder="Enter your password"
+                  />
+                  <svg className={s.icon} onClick={togglePasswordVisibility}>
+                    <use
+                      href={`${icons}#${
+                        showPassword ? "icon-view" : "icon-hide"
+                      }`}
+                    />
+                  </svg>
+                </label>
+              </li>
+            </ul>
+            <button className={s.btn} type="submit">
+              Sing in
+            </button>
+            <div className={s.no_account}>
+              <p className={s.text}>Don’t have an account?</p>
+              <Link to="/signup" className={s.link} type="submit">
+                Sign Up
+              </Link>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
