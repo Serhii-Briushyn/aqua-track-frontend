d23740991b0d9424df449afcf250a46dd2059334
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2025-01-09 21:18:23 +0200

    add

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,27 +1,35 @@
 import styles from "./WaterProgressBar.module.css";
 import { useEffect, useState } from "react";
 
-const WaterProgressBar = ({ progress, date }) => {
+const WaterProgressBar = ({ consumed, dailyGoal, date }) => {
   const [waterLevel, setWaterLevel] = useState(0);
   const [formattedDate, setFormattedDate] = useState("");
 
   useEffect(() => {
+    const progress = (consumed / dailyGoal) * 100;
     setWaterLevel(progress);
+
     const today = new Date();
     const todayFormatted = today.toLocaleDateString(navigator.language);
     const inputDate = new Date(date);
     const inputDateFormatted = inputDate.toLocaleDateString(navigator.language);
+
     if (inputDateFormatted === todayFormatted) {
       setFormattedDate("Today");
     } else {
       setFormattedDate(inputDate.getDate());
     }
-  }, [progress, date]);
+  }, [consumed, dailyGoal, date]);
   return (
-    <div className={styles.swater_progress_bar}>
+    <div className={styles.swater_progress_bar_wrapper}>
       <div className={styles.todayLabel}>{formattedDate}</div>
-      <div className={styles.water} style={{ width: `${waterLevel}%` }} />
-      <div className={styles.movingCircle} style={{ left: `${waterLevel}%` }} />
+      <div className={styles.waterProgressBar}>
+        <div className={styles.water} style={{ width: `${waterLevel}%` }} />
+        <div
+          className={styles.movingCircle}
+          style={{ left: `${waterLevel}%` }}
+        />
+      </div>
       <div className={styles.marks}>
         <span className={styles.mark}>0%</span>
         <span className={styles.mark}>50%</span>
