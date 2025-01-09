2ac3f9e46a159e1c1bd5053fd98635eef783c33d
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2025-01-09 21:11:17 +0100

    update signup form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,105 @@
+import { Formik, Form, Field } from "formik";
+import { Link } from "react-router-dom";
+import s from "./SignUpForm.module.css";
+import { useState } from "react";
+import icons from "../../assets/icons/icons.svg";
+import Logo from "../Logo/Logo";
+// import { useDispatch } from "react-redux";
+// import { register } from "../../redux/auth/operations";
+
 const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  // const dispatch = useDispatch();
+
+  // const initialValues = {
+  //   email: "",
+  //   password: "",
+  // };
+
+  // const handleSubmit = (values, options) => {
+  //   if (!values.email || !values.password || !values.password) {
+  //     return console.log("hello");
+  //   }
+  //   dispatch(register(values));
+  //   options.resetForm();
+  // };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div>
+      <div className={s.signin_wrapper}>
+        <Logo />
+        <div className={s.signin_form}>
+          <h2 className={s.title}>Sign In</h2>
+          <Formik>
+            <Form className={s.form}>
+              <ul className={s.list}>
+                <li>
+                  <label className={s.label}>
+                    Email
+                    <Field
+                      name="email"
+                      type="email"
+                      className={s.input}
+                      placeholder="Enter your email"
+                    />
+                  </label>
+                </li>
+                <li>
+                  <label className={`${s.password} ${s.label}`}>
+                    Password
+                    <Field
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      className={`${s.password} ${s.input}`}
+                      placeholder="Enter your password"
+                    />
+                    <svg className={s.icon} onClick={togglePasswordVisibility}>
+                      <use
+                        href={`${icons}#${
+                          showPassword ? "icon-view" : "icon-hide"
+                        }`}
+                      />
+                    </svg>
+                  </label>
+                </li>
+                <li>
+                  <label className={`${s.password} ${s.label}`}>
+                    Repeat password
+                    <Field
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      className={`${s.password} ${s.input}`}
+                      placeholder="Repeat password"
+                    />
+                    <svg className={s.icon} onClick={togglePasswordVisibility}>
+                      <use
+                        href={`${icons}#${
+                          showPassword ? "icon-view" : "icon-hide"
+                        }`}
+                      />
+                    </svg>
+                  </label>
+                </li>
+              </ul>
+              <button className={s.btn} type="submit">
+                Sing up
+              </button>
+              <div className={s.no_account}>
+                <p className={s.text}>Already have account?</p>
+                <Link to="/signin" className={s.link} type="submit">
+                  Sign In
+                </Link>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpForm;
