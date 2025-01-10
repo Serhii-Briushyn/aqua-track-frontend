a76fa6c7cefba579071ac3c30f472f759063fcc1
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-10 12:39:59 +0100

    update section

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 // import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App.jsx";
 // import { persistor, store } from "./redux/store.js";
-
+import { store } from "./redux/store.js";
 import "modern-normalize";
 import "./index.css";
 import "./fonts.css";
@@ -15,15 +15,15 @@ import "./fonts.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
-          <HelmetProvider>
-        <App />
-              {/* <DeleteWaterModal/> */}
-          </HelmetProvider>
-        </BrowserRouter>
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+          {/* <DeleteWaterModal/> */}
+        </HelmetProvider>
+      </BrowserRouter>
       {/* </PersistGate> */}
-    {/* </Provider> */}
+    </Provider>
   </StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { authReducer } from "./auth/slice";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: [],
+};
+
+export const store = configureStore({
+  reducer: {
+    user: persistReducer(authPersistConfig, authReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
