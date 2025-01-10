1d877ac56d54fccc1f2f360638fef1572ee3c178
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2025-01-10 21:30:37 +0100

    add errors message

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,28 +1,44 @@
-import { Formik, Form, Field } from "formik";
+/* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
 import s from "./SignUpForm.module.css";
 import { useState } from "react";
 import icons from "../../assets/icons/icons.svg";
-import Logo from "../Logo/Logo";
 import { useDispatch } from "react-redux";
-import { register } from "../../redux/auth/operations";
-// import { useDispatch } from "react-redux";
-// import { register } from "../../redux/auth/operations";
+import { register as signUp } from "../../redux/auth/operations";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
 
-  const initialValues = {
-    email: "",
-    password: "",
-  };
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Must contain at least 6 characters")
+      .max(64, "Password can't be longer than 64 characters")
+      .required("Password is required"),
+    repeatPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "The passwords do not match")
+      .required("Repeat password is required"),
+  });
 
-  const handleSubmit = (values, options) => {
-    if (!values.email || !values.password || !values.password) {
-      return console.log("hello");
-    }
-    dispatch(register(values));
-    options.resetForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => {
+    const { repeatPassword, ...filteredData } = data;
+    console.log("Data:", filteredData);
+    dispatch(signUp(data.email, data.password));
+    reset();
   };
 
   const [showPassword, setShowPassword] = useState(false);
@@ -38,72 +54,103 @@ const SignUpForm = () => {
   };
   return (
     <div>
-      <div className={s.signin_wrapper}>
-        <Logo />
-        <div className={s.signin_form}>
+      <div className={s.signup_wrapper}>
+        <div className={s.signup_form}>
           <h2 className={s.title}>Sign Up</h2>
-          <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-            <Form className={s.form}>
+          <form noValidate onSubmit={handleSubmit(onSubmit)}>
+            <div className={s.form}>
               <ul className={s.list}>
                 <li>
                   <label className={s.label}>
                     Email
-                    <Field
+                    <input
                       name="email"
                       type="email"
-                      className={s.input}
+                      {...register("email")}
+                      className={
+                        errors.email?.message ? s.input_error : s.input
+                      }
                       placeholder="Enter your email"
                     />
+                    {errors.email && (
+                      <p className={s.errors}>{errors.email.message}</p>
+                    )}
                   </label>
                 </li>
                 <li>
                   <label className={`${s.password} ${s.label}`}>
                     Password
-                    <Field
-                      name="password"
-                      type={showPassword ? "text" : "password"}
-                      className={`${s.password} ${s.input}`}
-                      placeholder="Enter your password"
-                    />
-                    <svg className={s.icon} onClick={togglePasswordVisibility}>
-                      <use
-                        href={`${icons}#${
-                          showPassword ? "icon-view" : "icon-hide"
-                        }`}
+                    <div className={s.input_wrapper}>
+                      <input
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                        {...register("password")}
+                        className={
+                          errors.password?.message ? s.input_error : s.input
+                        }
+                        placeholder="Enter your password"
                       />
-                    </svg>
+                      <svg
+                        className={s.icon}
+                        onClick={togglePasswordVisibility}
+                      >
+                        <use
+                          href={`${icons}#${
+                            showPassword ? "icon-view" : "icon-hide"
+                          }`}
+                        />
+                      </svg>
+                    </div>
+                    {errors.password && (
+                      <p className={s.errors}>{errors.password.message}</p>
+                    )}
                   </label>
                 </li>
                 <li>
                   <label className={`${s.password} ${s.label}`}>
                     Repeat password
-                    <Field
-                      name="repeat"
-                      type={showPasswordRepeat ? "text" : "password"}
-                      className={`${s.password} ${s.input}`}
-                      placeholder="Repeat password"
-                    />
-                    <svg className={s.icon} onClick={togglePasswordRVisibility}>
-                      <use
-                        href={`${icons}#${
-                          showPasswordRepeat ? "icon-view" : "icon-hide"
-                        }`}
+                    <div className={s.input_wrapper}>
+                      <input
+                        name="repeat"
+                        type={showPasswordRepeat ? "text" : "password"}
+                        {...register("repeatPassword")}
+                        className={
+                          errors.repeatPassword?.message
+                            ? s.input_error
+                            : s.input
+                        }
+                        placeholder="Repeat password"
                       />
-                    </svg>
+                      <svg
+                        className={s.icon}
+                        onClick={togglePasswordRVisibility}
+                      >
+                        <use
+                          href={`${icons}#${
+                            showPasswordRepeat ? "icon-view" : "icon-hide"
+                          }`}
+                        />
+                      </svg>
+                    </div>
+                    {errors.repeatPassword && (
+                      <p className={s.errors}>
+                        {errors.repeatPassword.message}
+                      </p>
+                    )}
                   </label>
                 </li>
               </ul>
               <button className={s.btn} type="submit">
                 Sing up
               </button>
-              <div className={s.no_account}>
-                <p className={s.text}>Already have account?</p>
-                <Link to="/signin" className={s.link} type="submit">
-                  Sign In
-                </Link>
-              </div>
-            </Form>
-          </Formik>
+            </div>
+          </form>
+          <div className={s.no_account}>
+            <p className={s.text}>Already have account?</p>
+            <Link to="/signin" className={s.link} type="submit">
+              Sign In
+            </Link>
+          </div>
         </div>
       </div>
     </div>
