db12730d3321ca82906771a761a52373218cb151
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2025-01-11 21:59:03 +0200

    add

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -6,27 +6,30 @@ import { useState } from "react";
 import s from "./AddWaterBtn.module.css";
 const AddWaterBtn = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalSource, setModalSource] = useState("");
+
   const openModal = () => {
+    setModalSource("AddWater");
     setIsModalOpen(true);
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
+    setModalSource("");
   };
   return (
     <div>
       <button className={s.button} onClick={openModal}>
-        <svg
-          className={s.icon}
-          aria-hidden="true"
-          viewBox="0 0 16 16"
-          width="16"
-          height="16"
-        >
-          <use xlinkHref="/src/assets/icons/icons.svg#icon-plus-circle-black" />
+        <svg className={s.icon}>
+          <use href="/src/assets/icons/icons.svg#icon-plus-circle-black" />
         </svg>{" "}
         Add water
       </button>
-      {isModalOpen && <WaterModal onClose={closeModal} />}
+      <WaterModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        source={modalSource}
+      />
     </div>
   );
 };
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,17 +1,19 @@
-import  WaterForm  from "../WaterForm/WaterForm";
+import Modal from "../Modal/Modal";
+import WaterForm from "../WaterForm/WaterForm";
 import css from "./WaterModal.module.css";
 
-const WaterModal = ({ title, subtitle, onClose, toggleHandle, water }) => {
+const WaterModal = ({ isOpen, onClose, source }) => {
+  const getTitle = () => {
+    if (source === "AddWater") return "Add water";
+    if (source === "EditWater") return "Edit the entered amount of water";
+    return "Default Title";
+  };
+
   return (
-    <div className={css.wrapper}>
-      <h2 className={css.title}>{title}</h2>
-      <WaterForm
-        subtitle={subtitle}
-        onClose={onClose}
-        toggleHandle={toggleHandle}
-        water={water}
-      />
-    </div>
+    <Modal isOpen={isOpen} onClose={onClose} className={css.wrapper}>
+      <h2 className={css.title}>{getTitle()}</h2>
+      <WaterForm isOpen={isOpen} onClose={onClose} />
+    </Modal>
   );
 };
 
