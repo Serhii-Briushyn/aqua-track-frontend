d9224b2a46c89e22a515cc586c57327e04eeaf80
Iryna R <deutsche.post15@gmail.com>
2025-01-12 17:27:45 +0100

    fix count

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,11 +1,8 @@
 import s from "./AdvantagesSection.module.css";
 import { selectUserCount, selectIsLoading } from "../../redux/auth/selectors";
-import {
-  // useDispatch,
-  useSelector
-} from "react-redux";
-// import { useEffect } from "react";
-// import { getUserCount } from "../../redux/auth/operations";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUserCount } from "../../redux/auth/operations";
 import Loader from "../Loader/Loader";
 import icons from "../../assets/icons/icons.svg";
 
@@ -24,12 +21,13 @@ import cust3Tab2x from "../../assets/images/3-customers-desk-tab@2x-min.png";
 const AdvantagesSection = () => {
   const usersCount = useSelector(selectUserCount);
   const usersLoading = useSelector(selectIsLoading);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(getUserCount());
-  // }, [dispatch]);
+  useEffect(() => {
+    dispatch(getUserCount());
+  }, [dispatch]);
 
+  console.log("Users Count:", usersCount);
   return (
     <div className={s.main}>
       <div className={s.customers}>
