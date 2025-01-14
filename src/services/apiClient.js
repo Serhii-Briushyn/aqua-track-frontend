c14d2335e9dbb468ae4d3f9b69934dfb5168f62e
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-14 05:37:39 +0100

    baseURL

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 
 export const aquaTrackApi = axios.create({
-  // baseURL: "https://aqua-track-backend-4vct.onrender.com",
-  baseURL: "https://aqua-track-backend-4vct.onrender.com",
+  baseURL: "https://aqua-track-backend-moj8.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export const publicApi = axios.create({
-  baseURL: "https://aqua-track-backend-4vct.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
