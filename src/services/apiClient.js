052bfd14349ddaef673808fca7ce89dbee0bc1c4
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-14 05:38:09 +0100

    api

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const aquaTrackApi = axios.create({
-  baseURL: "https://aqua-track-backend-moj8.onrender.com",
+  baseURL: "https://aqua-track-backend-moj8.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
