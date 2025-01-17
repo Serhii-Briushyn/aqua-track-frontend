7ea9ef5a88b07c8bbeb52b6ff0b6411760972aee
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-17 23:01:37 +0100

    notFoundImage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,10 @@
 import css from "./NotFoundPage.module.css";
+import notFoundImage from "../assets/images/not-found-image.png";
 
 const NotFoundPage = () => {
   return (
     <div className={css.wrapper}>
-      <img className={css.image} src="/src/assets/images/not-found-image.png" />
+      <img className={css.image} src={notFoundImage} alt="Not Found" />
     </div>
   );
 };
