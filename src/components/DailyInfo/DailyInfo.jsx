3f91d97d091ab1416eb64175229d75ca0ffb14bd
Andrey Drapatiy <drapatiy92@gmail.com>
2025-01-20 17:56:29 +0200

    Current date

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -2,11 +2,25 @@ import AddWaterBtn from "../AddWaterBtn/AddWaterBtn";
 import WaterList from "../WaterList/WaterList";
 import css from "./DailyInfo.module.css";
 import { CircularProgress } from "@mui/material";
+
 const DailyInfo = ({ dailyData, isLoading, selectedDate, onSubmitSuccess }) => {
+  const getHeaderTitle = () => {
+    const today = new Date();
+    const isToday =
+      today.toDateString() === new Date(selectedDate).toDateString();
+
+    if (isToday) {
+      return "Today";
+    }
+
+    const options = { day: "numeric", month: "long" };
+    return new Intl.DateTimeFormat("en-US", options).format(selectedDate);
+  };
+
   return (
     <div>
       <div className={css.infoHeader}>
-        <h2 className={css.h2}>Today</h2>
+        <h2 className={css.h2}>{getHeaderTitle()}</h2>
         <AddWaterBtn
           type="waterDetail"
           selectedDate={selectedDate}
@@ -18,10 +32,7 @@ const DailyInfo = ({ dailyData, isLoading, selectedDate, onSubmitSuccess }) => {
           <CircularProgress color="#9be1a0" />
         </div>
       ) : (
-        <WaterList
-          dailyData={dailyData}
-          onSubmitSuccess={onSubmitSuccess}
-        />
+        <WaterList dailyData={dailyData} onSubmitSuccess={onSubmitSuccess} />
       )}
     </div>
   );
