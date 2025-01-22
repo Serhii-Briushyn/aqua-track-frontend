496b13f6db1c840906c68d9dabc614f0ff48f628
Serhii Briushyn <serhii.briushyn@icloud.com>
2025-01-22 11:05:51 +0100

    translation calendar

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,23 +1,37 @@
-import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { useTranslation } from "react-i18next";
+
+import {
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
+
 import css from "../CalendarPagination/CalendarPagination.module.css";
 
 const CalendarPagination = ({ handleMonthChange, currentMonth }) => {
+  const { i18n } = useTranslation();
+
   return (
     <div className={css.pagination}>
       <button
         className={css.arrowBtn}
         onClick={() => handleMonthChange(-1)} // Move to the previous month
       >
-        <MdOutlineKeyboardArrowLeft style={{ fontSize: '22px', color: '#323F47' }} />
+        <MdOutlineKeyboardArrowLeft
+          style={{ fontSize: "22px", color: "#323F47" }}
+        />
       </button>
       <div className={css.date}>
-        {currentMonth.toLocaleString('en-US', { month: 'long' }) + ', ' + currentMonth.getFullYear()}
+        {currentMonth.toLocaleString(i18n.language, { month: "long" }) +
+          ", " +
+          currentMonth.getFullYear()}
       </div>
       <button
         className={css.arrowBtn}
         onClick={() => handleMonthChange(1)} // Move to the next month
       >
-        <MdOutlineKeyboardArrowRight style={{ fontSize: '22px', color: '#323F47' }} />
+        <MdOutlineKeyboardArrowRight
+          style={{ fontSize: "22px", color: "#323F47" }}
+        />
       </button>
     </div>
   );
--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,46 +1,58 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 import Calendar from "../Calendar/Calendar";
-import {useState} from "react";
-import css from "./MonthInfo.module.css";
 import CalendarPagination from "../CalendarPagination/CalendarPagination.jsx";
 import icons from "../../assets/icons/icons.svg";
 import Chart from "../Chart/Chart.jsx";
 
-const MonthInfo = ({ monthlyData, selectedDate, currentMonth, onChangeMonth, onChangeDate }) => {
-	const [isCalendarView, setIsCalendarView] = useState(true);
+import css from "./MonthInfo.module.css";
 
-	return (
-		<div className={css.monthInfo}>
-			<div className={css.headingPanel}>
-				<h2 className={css.h2}>{isCalendarView ? 'Month' : 'Statistics'}</h2>
-				<div className={css.infoNav}>
-					<CalendarPagination
-						selectedDate={selectedDate}
-						currentMonth={currentMonth}
-						handleMonthChange={onChangeMonth}
-					/>
-					<button
-						className={css.toggleView}
-						onClick={() => setIsCalendarView(!isCalendarView)}
-					>
-						<svg>
-							<use href={`${icons}#icon-chart`}/>
-						</svg>
-					</button>
-				</div>
-			</div>
-			{isCalendarView ?
-				<Calendar
-					currentMonth={currentMonth}
-					selectedDate={selectedDate}
-					changeMonth={onChangeMonth}
-					monthValues={monthlyData}
-					onDateSelect={(date) => onChangeDate(date)}
-				/>
-				:
-				<Chart data={monthlyData} />
-			}
-		</div>
-	);
+const MonthInfo = ({
+  monthlyData,
+  selectedDate,
+  currentMonth,
+  onChangeMonth,
+  onChangeDate,
+}) => {
+  const [isCalendarView, setIsCalendarView] = useState(true);
+  const { t } = useTranslation();
+
+  return (
+    <div className={css.monthInfo}>
+      <div className={css.headingPanel}>
+        <h2 className={css.h2}>
+          {isCalendarView ? t("month") : t("statistics")}
+        </h2>
+        <div className={css.infoNav}>
+          <CalendarPagination
+            selectedDate={selectedDate}
+            currentMonth={currentMonth}
+            handleMonthChange={onChangeMonth}
+          />
+          <button
+            className={css.toggleView}
+            onClick={() => setIsCalendarView(!isCalendarView)}
+          >
+            <svg>
+              <use href={`${icons}#icon-chart`} />
+            </svg>
+          </button>
+        </div>
+      </div>
+      {isCalendarView ? (
+        <Calendar
+          currentMonth={currentMonth}
+          selectedDate={selectedDate}
+          changeMonth={onChangeMonth}
+          monthValues={monthlyData}
+          onDateSelect={(date) => onChangeDate(date)}
+        />
+      ) : (
+        <Chart data={monthlyData} />
+      )}
+    </div>
+  );
 };
 
 export default MonthInfo;
