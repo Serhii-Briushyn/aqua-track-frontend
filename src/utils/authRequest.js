b4561f9f32a219c1597a8b4c2056cea21d4be257
Serhii Briushyn <briushyn.developer@gmail.com>
2025-06-19 22:41:28 +0200

    return

--- src/utils/authRequest.js
+++ src/utils/authRequest.js
@@ -1,9 +1,6 @@
 import { refreshAccessToken } from "../redux/auth/operations";
 import { clearAccessToken } from "../redux/auth/slice";
 
-let isRefreshing = false;
-let refreshPromise = null;
-
 export const authRequest = async (axiosFn, thunkAPI) => {
   try {
     return await axiosFn();
@@ -11,18 +8,8 @@ export const authRequest = async (axiosFn, thunkAPI) => {
     const status = error?.response?.status;
 
     if (status === 401) {
-      if (!isRefreshing) {
-        isRefreshing = true;
-        refreshPromise = thunkAPI
-          .dispatch(refreshAccessToken())
-          .unwrap()
-          .finally(() => {
-            isRefreshing = false;
-          });
-      }
-
       try {
-        await refreshPromise;
+        await thunkAPI.dispatch(refreshAccessToken()).unwrap();
         return await axiosFn();
       } catch {
         thunkAPI.dispatch(clearAccessToken());
